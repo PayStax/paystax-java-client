889392f658de1398d1419a2c27bc58578c38094e
Erik R. Jensen <ejensen@netradius.com>
2015-06-29 16:19:47 -0600

    Updating license headers.

--- src/main/java/com/paystax/client/Version.java
+++ src/main/java/com/paystax/client/Version.java
@@ -4,6 +4,6 @@ package com.paystax.client;
  * @author Erik R. Jensen
  */
 public class Version {
-	public static final String BUILDTIME = "2015-06-29T14:51:23Z";
+	public static final String BUILDTIME = "2015-06-29T22:19:40Z";
 	public static final String VERSION = "1.0.1-SNAPSHOT";
 }
