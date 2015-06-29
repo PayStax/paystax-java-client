98e7696900787674458afcd2d67937937da968fb
Erik R. Jensen <ejensen@netradius.com>
2015-06-29 08:51:46 -0600

    Incrementing version for release

--- src/main/java/com/paystax/client/Version.java
+++ src/main/java/com/paystax/client/Version.java
@@ -4,6 +4,6 @@ package com.paystax.client;
  * @author Erik R. Jensen
  */
 public class Version {
-	public static final String BUILDTIME = "2015-06-29T14:50:36Z";
-	public static final String VERSION = "1.0.0";
+	public static final String BUILDTIME = "2015-06-29T14:51:23Z";
+	public static final String VERSION = "1.0.1-SNAPSHOT";
 }
