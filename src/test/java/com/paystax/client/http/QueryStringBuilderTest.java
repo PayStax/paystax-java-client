3fe1d4cfdd57844af14842ee12558872a978e72d
Erik R. Jensen <ejensen@netradius.com>
2015-06-29 08:02:26 -0600

    Adding unit test for QuryStringBuilder.

---
+++ src/test/java/com/paystax/client/http/QueryStringBuilderTest.java
@@ -0,0 +1,45 @@
+package com.paystax.client.http;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author Erik R. Jensen
+ */
+public class QueryStringBuilderTest {
+
+	@Data
+	@Accessors(chain = true)
+	public class TestObject {
+		private String test1;
+		private String test2;
+	}
+
+	@Test
+	public void testToQueryString() {
+		QueryStringBuilder qb = new QueryStringBuilder();
+		qb.add(new TestObject().setTest1("test1").setTest2("test2"));
+		String queryString = qb.toQueryString();
+		assertThat(queryString, equalTo("test1=test1&test2=test2"));
+	}
+
+	@Test
+	public void testEncoddedQueryString1() {
+		QueryStringBuilder qb = new QueryStringBuilder();
+		qb.add("test1", "m&m");
+		String queryString = qb.toQueryString();
+		assertThat(queryString, equalTo("test1=m%26m"));
+	}
+
+	@Test
+	public void testEncodedQuwryString2() {
+		QueryStringBuilder qb = new QueryStringBuilder();
+		qb.add(new TestObject().setTest1("m&m").setTest2("x=y"));
+		String queryString = qb.toQueryString();
+		assertThat(queryString, equalTo("test1=m%26m&test2=x%3Dy"));
+	}
+}
