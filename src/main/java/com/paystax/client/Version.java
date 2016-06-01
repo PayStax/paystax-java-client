9bb32648e17a3e7df6b16f7f1562a560ca1703db
Erik R. Jensen <ejensen@netradius.com>
2016-06-01 16:29:01 -0600

    Adding stuff.

--- src/main/java/com/paystax/client/Version.java
+++ src/main/java/com/paystax/client/Version.java
@@ -4,6 +4,6 @@ package com.paystax.client;
  * @author Erik R. Jensen
  */
 public class Version {
-	public static final String BUILDTIME = "2016-02-08T03:35:04Z";
-	public static final String VERSION = "1.0.1";
+	public static final String BUILDTIME = "2016-02-10T04:55:21Z";
+	public static final String VERSION = "1.0.2-SNAPSHOT";
 }
