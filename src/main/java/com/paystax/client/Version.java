2ffad8b4b834c0048c3805620c76f67a33cc371e
Erik R. Jensen <ejensen@netradius.com>
2016-08-15 19:48:53 -0600

    Removing Version.java file.

--- src/main/java/com/paystax/client/Version.java
+++
@@ -1,9 +0,0 @@
-package com.paystax.client;
-
-/**
- * @author Erik R. Jensen
- */
-public class Version {
-	public static final String BUILDTIME = "2016-08-15T04:23:56Z";
-	public static final String VERSION = "1.0.2-SNAPSHOT";
-}
