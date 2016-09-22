03517153623648d982448604699c2e7a06272414
Erik R. Jensen <ejensen@netradius.com>
2016-09-22 07:39:20 -0600

    Adding license headers

---
+++ src/main/java/com/paystax/client/gateway/PayStaxWirecardGateway.java
@@ -0,0 +1,50 @@
+/**
+ *    Copyright 2013-2016 PayStax, LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.paystax.client.gateway;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.paystax.client.http.RestClient;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * @author Erik R. Jensen
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeName("WIRECARD")
+public class PayStaxWirecardGateway extends PayStaxGateway<PayStaxWirecardGateway> {
+
+	private static final long serialVersionUID = -1541330244104330932L;
+
+	protected String url;
+	protected String username;
+	protected String password;
+	protected String merchantId;
+
+	public PayStaxWirecardGateway(RestClient restClient) {
+		super(restClient);
+		this.type = PayStaxGatewayType.WIRECARD;
+	}
+}
---
+++ src/main/java/com/paystax/client/transaction/PayStaxSEPA.java
@@ -0,0 +1,62 @@
+/**
+ *    Copyright 2013-2016 PayStax, LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.paystax.client.transaction;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.paystax.client.http.RestClient;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+/**
+ * @author Erik R. Jensen
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeName("SEPA")
+public class PayStaxSEPA extends PayStaxTransaction<PayStaxSEPA> {
+
+	private static final long serialVersionUID = -6445650206785154459L;
+
+	protected UUID paymentMethodId;
+	protected BigDecimal amount;
+
+	public PayStaxSEPA(RestClient restClient) {
+		super(restClient);
+		this.type = PayStaxTransactionType.SEPA;
+	}
+
+	public PayStaxSEPA setGatewayId(UUID gatewayId) {
+		this.gatewayId = gatewayId;
+		return this;
+	}
+
+	public PayStaxSEPA setMerchantReference(String merchantReference) {
+		this.merchantReference = merchantReference;
+		return this;
+	}
+
+}
